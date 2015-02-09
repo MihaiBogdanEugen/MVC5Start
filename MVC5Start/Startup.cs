e4d70208e539efef0e5b1903395452d4b83b0c93
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2015-02-09 15:34:40 +0200

    Added a default MVC5 project, updated packages and moved some classes into separate files.

---
+++ MVC5Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using MVC5Start;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Startup))]
+namespace MVC5Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder application)
+        {
+            ConfigureAuthentication(application);
+        }
+    }
+}
