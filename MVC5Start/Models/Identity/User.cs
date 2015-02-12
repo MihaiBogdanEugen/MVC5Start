78f0f795a71460db2ca980b2ecd3a3173f2771ec
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2015-02-12 13:54:07 +0200

    [BUILD-NOT-WORKING]  Added missing comments on the User model class.

--- MVC5Start/Models/Identity/User.cs
+++ MVC5Start/Models/Identity/User.cs
@@ -18,38 +18,95 @@ namespace MVC5Start.Models.Identity
             this.Logins = new List<UserLogin>();
         }  
 
+        /// <summary>
+        /// The first name of the user.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The last name of the user.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The username of the user. 
+        /// Usually, the same as the email.
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// The password hash of the user.
+        /// </summary>
         public string PasswordHash { get; set; }
 
+        /// <summary>
+        /// The security timestamp introduced by the Identity engine of the user.
+        /// </summary>
         public string SecurityStamp { get; set; }
 
+        /// <summary>
+        /// The email of the user.
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Flag indicating if the email is confirmed or not.
+        /// </summary>
         public bool EmailConfirmed { get; set; }
 
+        /// <summary>
+        /// The phone number of the user.
+        /// </summary>
         public string PhoneNumber { get; set; }
 
+        /// <summary>
+        /// Flag indicating if the phone number is confirmed or not.
+        /// </summary>
         public bool PhoneNumberConfirmed { get; set; }
 
+        /// <summary>
+        /// Flag indicating if two factor authentication is enabled or not for the current user.
+        /// </summary>
         public bool TwoFactorEnabled { get; set; }
 
+        /// <summary>
+        /// The UTC timestamp when the lockout will end.
+        /// </summary>
         public DateTime? LockoutEndDateUtc { get; set; }
 
+        /// <summary>
+        /// Flag indicating if lockout is enabled or not.
+        /// </summary>
         public bool LockoutEnabled { get; set; }
 
+        /// <summary>
+        /// Counter of failed login attempts.
+        /// </summary>
         public int AccessFailedCount { get; set; }
 
+        /// <summary>
+        /// Timestamp of last successfull login.
+        /// </summary>
         public DateTime? LastLogInAtUtc { get; set; }
 
+        /// <summary>
+        /// Flag indicating if the current user is disabled or not.
+        /// </summary>
+        public bool IsDisabled { get; set; }
+
+        /// <summary>
+        /// The list of UserRoles of the current user.
+        /// </summary>
         public virtual ICollection<UserRole> Roles { get; private set; }
 
+        /// <summary>
+        /// The list of claims of the current user.
+        /// </summary>
         public virtual ICollection<UserClaim> Claims { get; private set; }
 
+        /// <summary>
+        /// The list of logins of the current user.
+        /// </summary>
         public virtual ICollection<UserLogin> Logins { get; private set; }
     }
 }
